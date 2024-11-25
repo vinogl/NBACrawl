8143a0e78b3e5ce8b5f0d2b30070396c8eb6a155
gongli <gongli.vino@gmail.com>
2024-11-25 20:09:13 +0800

    default

--- GetTrend.py
+++ GetTrend.py
@@ -27,19 +27,17 @@ def get_trend(trend_route: str, game_time: datetime, game_id: int, co: str) -> l
     trend_all = trend_table.find_all('tr')  # 提取表格中的所有行
 
     # 取出表格的第一行日期作为基准日期，表格中的所有日期均不大于第一行的日期
-    row_base_time = datetime.strptime(trend_all[1].find_all('td')[-1].get_text().replace("(走地)", "").strip(), "%m-%d %H:%M")
+    row_base_time = datetime.strptime("%d-%s" % (game_time.year, trend_all[1].find_all('td')[-1].get_text().replace("(走地)", "").strip()), "%Y-%m-%d %H:%M")
 
     trend_list = []
     for row in trend_all[1:]:
         row_data = [cell.get_text() for cell in row.find_all('td')]
 
         # 补全日期年份
-        row_time = datetime.strptime(row_data[-1].replace("(走地)", "").strip(), "%m-%d %H:%M")
+        row_time = datetime.strptime("%d-%s" % (game_time.year, row_data[-1].replace("(走地)", "").strip()), "%Y-%m-%d %H:%M")
         if row_time > row_base_time or row_time.month > game_time.month:
             # 日期若大于第一行或者日期月份比比赛时间月份大，则说明这个日期应该是前一年的日期
             row_time = row_time.replace(year=game_time.year - 1)
-        else:
-            row_time = row_time.replace(year=game_time.year)
 
         if row_time < game_time:
             row_data.pop()
